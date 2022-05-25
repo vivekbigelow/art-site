fb0425e62f14f6c78e6c689c3969b56abb69e74d
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-05-25 16:49:36 -0400

    App form adds new piece.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [pieces, setPieces] = useState([])
-  const [newPiece, setNewPiece] = useState({})
+  const [pieces, setPieces] = useState([]);
+  const [newPiece, setNewPiece] = useState({ title: "", medium: "", year: "" });
 
   const addPiece = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    console.log("newPiece", newPiece);
     const pieceObject = {
       title: newPiece.title,
       year: newPiece.year,
@@ -17,56 +18,70 @@ function App() {
       image: "/src/assets/static/images/smiley.png",
     };
 
-    axios
-    .post("http://localhost:3001/cards", pieceObject)
-    .then(response => {
-      setPieces(pieces.concat(response.data))
-    })
+    axios.post("http://localhost:3001/cards", pieceObject).then((response) => {
+      setPieces(pieces.concat(response.data));
+    });
 
-    setNewPiece({})
-  }
+    setNewPiece({ title: "", medium: "", year: "" });
+  };
 
-  const handleNotChange = (event) => {
-    setNewPiece(event.target.value)
-  }
+  const handleTitleChange = (e) => {
+    let updatedValue = {};
+    updatedValue = { title: e.target.value };
+    setNewPiece((newPiece) => ({ ...newPiece, ...updatedValue }));
+  };
 
-  
+  const handleMediumChange = (e) => {
+    let updatedValue = {};
+    updatedValue = { medium: e.target.value };
+    setNewPiece((newPiece) => ({ ...newPiece, ...updatedValue }));
+  };
+
+  const handleYearChange = (e) => {
+    let updatedValue = {};
+    updatedValue = { year: e.target.value };
+    setNewPiece((newPiece) => ({ ...newPiece, ...updatedValue }));
+  };
+
+  // const handlePieceChange = (e) => {
+  //   let updatedValue = {};
+  //   updatedValue = {title: e.target.value};
+  //   setNewPiece(newPiece => ({...newPiece, ...updateValue}));
+  // }
 
   useEffect(() => {
     // console.log('effect')
-    axios
-    .get("http://localhost:3001/cards")
-    .then((response) => {
+    axios.get("http://localhost:3001/cards").then((response) => {
       // console.log("promise fulfilled");
       setPieces(response.data);
     });
-  }, [])
+  }, []);
   //console.log('render', cards.length, 'cards');
-  
+
   return (
     <>
       <ChakraProvider>
-        <Home data={cards} />
+        <Home data={pieces} />
         <form onSubmit={addPiece}>
           <label for="title">Title:</label>
           <input
             value={newPiece.title}
             name="title"
-            onChange={handleNotChange}
+            onChange={handleTitleChange}
           />
           <label for="medium">Medium:</label>
           <input
             value={newPiece.medium}
             name="medium"
-            onChange={handleNotChange}
+            onChange={handleMediumChange}
           />
           <label for="year">Year:</label>
           <input
             value={newPiece.year}
             name="year"
-            onChange={handleNotChange}
+            onChange={handleYearChange}
           />
-          <button type="submit">Save</button>
+          <button type="submit">Add New Piece</button>
         </form>
       </ChakraProvider>
     </>
---
+++ src/components/PieceForm.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const PieceForm = () => {
+  const [newPiece, setNewPiece] = useState({});
+  return <div>PieceForm</div>;
+};
+
+export default PieceForm;
